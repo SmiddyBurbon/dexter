9261c6ec2f994ab4fd7d6318bc801578801d76d1
Dennis <me@dennisschmidt.net>
2021-06-03 22:36:31 +0200

    npm install vue-firebase

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase'
+import VueFirebase from 'vue-firebase'
 
-createApp(App).mount('#app')
+// Config firebase project
+const FBCONFIG = {
+    apiKey: "AIzaSyDsacsggn4PhHeufqVLc7Nvu5wySBK118U",
+    authDomain: "dexter-9cb27.firebaseapp.com",
+    databaseURL: "https://dexter-9cb27-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "dexter-9cb27",
+    storageBucket: "dexter-9cb27.appspot.com",
+    messagingSenderId: "104109341536"
+};
+ 
+Vue(App).use(VueFirebase, {firebase: firebase, config: FBCONFIG});
+
+
+Vue(App).mount('#app')
